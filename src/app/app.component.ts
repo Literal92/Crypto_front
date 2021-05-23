5330e3f9084a244a9b59a6d44d012fe2cb94bb93
tavakoli <you@example.com>
2021-05-23 13:14:19 +0430

    subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { MenuItem, PrimeNGConfig } from 'primeng/api';
 import { Product } from './product';
 import { SelectItem } from 'primeng/api';
 import { ProductService } from './productservice';
+import { Subscription, timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +14,22 @@ import { ProductService } from './productservice';
 export class AppComponent {
   items: MenuItem[];
 
-  products: Product[];
+  products: Product[] = [];
 
   sortOptions: SelectItem[];
 
   sortOrder: number = -1;
 
   sortField: string;
-
+  realTimeDataSubscription$: Subscription;
+  
   constructor(private productService: ProductService, private primengConfig: PrimeNGConfig) { }
 
   
   ngOnInit(): void {
-    this.products = [];
-    this.productService.getCrypto().subscribe(data =>{
+    this.realTimeDataSubscription$ = timer(0, 30000)
+    .pipe(switchMap(_ => this.productService.getCrypto()))
+    .subscribe(data => {
       this.products = data;
     });
     this.items = [
